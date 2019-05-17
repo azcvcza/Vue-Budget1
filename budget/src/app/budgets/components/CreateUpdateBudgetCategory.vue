82e2631fbb69ca6f83937f08869c114214ca16b3
韦粤信 <283940802@qq.com>
2019-05-17 23:55:20 +0800

    feat: create b&c .vue

---
+++ budget/src/app/budgets/components/CreateUpdateBudgetCategory.vue
@@ -0,0 +1,53 @@
+<template>
+ <div id="budget-category-create-edit-view">
+ <form class="form" @submit.prevent="processSave">
+ <div class="control is-horizontal">
+ <div class="control is-grouped">
+ <div class="control is-expanded">
+ <input type="text" v-model="budgetCategory.category">
+ </div>
+ <div class="control is-expanded">
+ #####replaceparse40#####lt;input type="number" class="input" v-model="budgetCategory.budgeted" />
+ </div>
+ <div class="control is-expanded">
+ {{ budgetCategory.spent }}
+ </div>
+ <button @click.prevent="processSave">Add</button>
+ </div>
+ </div>
+ </form>
+ </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+
+export default {
+ name: 'budget-categorycreate-edit-view',
+
+ components: {
+ },
+
+ data: () => {
+ return {
+ budgetCategory: {}
+ };
+ },
+
+ mounted () {
+ },
+
+ methods: {
+ ...mapActions([
+ ]),
+
+ processSave () {
+ }
+ },
+
+ computed: {
+ ...mapGetters([
+ ])
+ }
+};
+</script>
