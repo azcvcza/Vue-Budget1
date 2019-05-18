3bef030db21c60b4acac2b9cbd05c5a50f7d642e
韦粤信 <283940802@qq.com>
2019-05-18 18:21:19 +0800

    feat: modify updatepage

--- budget/src/app/budgets/components/CreateUpdateBudget.vue
+++ budget/src/app/budgets/components/CreateUpdateBudget.vue
@@ -1,82 +1,181 @@
 <template>
 	<div id="budget-create-edit-view">
-		You can create and edit budgets with me, woot!
+		<nav class="level">
+			<div class="level-left">
+				<h1 class="title is-2">Add Budget</h1>
+			</div>
+			<div class="level-right">
+				<div class="level-item">
+					<router-link :to="{ name: 'budgetsList' }" class="button">View all budgets &#8630;</router-link>
+				</div>
+			</div>
+		</nav>
+
 		<form class="form" @submit.prevent="processSave">
 			<label for="month" class="label">Month</label>
 			<p class="control">
 				<datepicker name="month" input-class="input" format="MMMM yyyy" v-model="selectedBudget.month"></datepicker>
+				<span class="icon">
+					<i class="fa fa-calendar" aria-hidden></i>
+				</span>
 			</p>
-			<label for="budgeted" class="label">Budgeted amount</label>
-			<p class="control">
-				<input type="text" class="input" name="budgeted" v-model="selectedBudget.budgeted">
-			</p>
-			<p class="control">Spent: {{ selectedBudget.spent }}</p>
-			<p class="control">Income: {{ selectedBudget.income }}</p>
-			
+			<p class="control">Budgeted: ${{ selectedBudget.budgeted }}</p>
+			<p class="control">Spent: ${{ selectedBudget.spent }}</p>
+			<p class="control">Income: ${{ selectedBudget.income }}</p>
+				<p class="help" v-if="!editing">To add budget items you must first save the budget.</p>
 			<div class="field is-grouped">
+				
 				<div class="control">
 					<button class="button is-link">Submit</button>
 				</div>
 				<div class="control">
+				
 					<router-link :to="{ name: 'budgetsList' }">
 						<button class="button is-warning">Cancel</button>
 					</router-link>
 				</div>
 			</div>
 		</form>
+
+		<div v-if="editing">
+			<nav class="level">
+				<div class="level-item has-text-centered">
+					<div>
+						<p class="heading">Budgeted</p>
+						<p class="title">${{ selectedBudget.budgeted }}</p>
+					</div>
+				</div>
+				<div class="level-item has-text-centered">
+					<div>
+						<p class="heading">Spent</p>
+						<p class="title">${{ selectedBudget.spent }}</p>
+					</div>
+				</div>
+				<div class="level-item has-text-centered">
+					<div>
+						<p class="heading">Income</p>
+						<p class="title">${{ selectedBudget.income }}</p>
+					</div>
+				</div>
+			</nav>
+
+			<table class="table is-bordered is-striped is-hoverable is-fullwidth">
+				<thead>
+					<tr>
+						<th>Category</th>
+						<th>Budgeted</th>
+						<th>Spent</th>
+						<th>Remaining</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr v-for="bc in selectedBudget.budgetCategories">
+						<td>
+							<span class="subtitle is-5">{{ getCategoryById(bc.category).name }}</span>
+						</td>
+						<td>
+							<span class="subtitle is-5">${{ bc.budgeted }}</span>
+						</td>
+						<td>
+							<span class="subtitle is-5">${{ bc.spent }}</span>
+						</td>
+						<td>
+							<span class="subtitle is-5">${{ bc.budgeted - bc.spent }}</span>
+						</td>
+					</tr>
+					<CreateUpdateBudgetCategory v-on:add-budget-category="addBudgetCategory"></CreateUpdateBudgetCategory>
+				</tbody>
+				<tfoot>
+					<tr>
+						<td></td>
+						<td>${{ selectedBudget.budgeted }}</td>
+						<td>${{ selectedBudget.spent }}</td>
+						<td>${{ selectedBudget.budgeted - selectedBudget.spent }}</td>
+					</tr>
+				</tfoot>
+			</table>
+		</div>
 	</div>
 </template>
 
 <script>
 	import { mapActions, mapGetters } from "vuex";
-	import Datepicker from 'vuejs-datepicker';
+	import Datepicker from "vuejs-datepicker";
+	import CreateUpdateBudgetCategory from "./CreateUpdateBudgetCategory";
 	export default {
 		name: "budget-create-edit-view",
+		components: {
+			Datepicker,
+			CreateUpdateBudgetCategory
+		},
 		data: () => {
 			return {
-				selectedBudget: {}
+				selectedBudget: {},
+				editing: false
 			};
-		},
-		components:{
-			Datepicker
 		},
 		mounted() {
 			if ("budgetId" in this.$route.params) {
 				this.loadBudgets().then(() => {
 					let selectedBudget = this.getBudgetById(this.$route.params.budgetId);
 					if (selectedBudget) {
+						this.editing = true;
 						this.selectedBudget = Object.assign({}, selectedBudget);
 					}
 				});
 			}
 		},
 		methods: {
-			...mapActions(["createBudget", "updateBudget", "loadBudgets"]),
+			...mapActions([
+				"createBudget",
+				"updateBudget",
+				"loadBudgets",
+				"createBudgetCategory"
+			]),
 			resetAndGo() {
 				this.selectedBudget = {};
-				// todo: redirect here
-				this.$router.push({name:'budgetsList'});
+				this.$router.push({ name: "budgetsList" });
 			},
 			saveNewBudget() {
-				this.createBudget(this.selectedBudget).then(() => {
-					this.resetAndGo();
-				}).catch((err)=>{
-					console.log("err in saveNewBudget:",err);
-				});
+				this.createBudget(this.selectedBudget)
+					.then(() => {
+						this.resetAndGo();
+					})
+					.catch(err => {
+						alert(err);
+					});
 			},
 			saveBudget() {
-				this.updateBudget(this.selectedBudget).then(() => {
-					this.resetAndGo();
-				}).catch((err)=>{
-					console.log("err in saveBudget:",err);
-				});
+				this.updateBudget(this.selectedBudget)
+					.then(() => {
+						this.resetAndGo();
+					})
+					.catch(err => {
+						alert(err);
+					});
 			},
 			processSave() {
 				this.$route.params.budgetId ? this.saveBudget() : this.saveNewBudget();
+			},
+			addBudgetCategory(budgetCategory) {
+				if (!budgetCategory.category) return;
+				this.createBudgetCategory({
+					budget: this.selectedBudget,
+					budgetCategory: {
+						category: budgetCategory.category.id,
+						budgeted: budgetCategory.budgeted,
+						spent: 0
+					}
+				}).then(() => {
+					this.selectedBudget = Object.assign(
+						{},
+						this.getBudgetById(this.$route.params.budgetId)
+					);
+				});
 			}
 		},
 		computed: {
-			...mapGetters(["getBudgetById"])
+			...mapGetters(["getBudgetById", "getCategoryById"])
 		}
 	};
 </script>
