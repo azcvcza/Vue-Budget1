a7a00287ab7871c29a414ae072a32a151d1e8551
韦粤信 <283940802@qq.com>
2019-05-20 21:25:03 +0800

    feat: update navigation with account

--- budget/src/app/navigation/components/Navigation.vue
+++ budget/src/app/navigation/components/Navigation.vue
@@ -1,30 +1,53 @@
 <template>
-  <div id="navigation-view">
-    <ul>
-      <li><router-link :to="{ name: 'accountsList' }">Accounts</router-link></li>
-      <li><router-link :to="{ name: 'budgetsList' }">Budgets</router-link></li>
-    </ul>
-  </div>
+	<div id="navigation-view">
+		<ul>
+			<li>
+				<router-link :to="{ name: 'accountsList' }">Accounts</router-link>
+				<ul>
+					<li v-for="account in accounts">
+						<router-link :to="{ name: 'editAccount', params: { accountId: account.id } }">
+							name:{{ account.name }}-<span>${{ account.balance }}</span>
+						</router-link>
+					</li>
+				</ul>
+			</li>
+			<li>
+				<router-link :to="{ name: 'budgetsList' }">Budgets</router-link>
+			</li>
+		</ul>
+	</div>
 </template>
 
 <script>
-export default {
-  name: 'navigation'
-};
+	import { mapActions, mapState } from "vuex";
+	export default {
+		name: "navigation",
+		mounted() {
+			this.loadAccounts();
+		},
+		methods: {
+			...mapActions(["loadAccounts"])
+		},
+		computed: {
+			...mapState({
+				accounts: state => state.accounts.accounts
+			})
+		}
+	};
 </script>
 
 <style scoped lang='scss'>
-  ul {
-    margin-top: 50px;
-    li {
-      border-bottom: 2px solid rgb(31, 78, 93);
-      font-size: 1.8em;
-      
-      
-      a {
-        color: #000000;
-      }
-    }
-  }
-	
+a {
+	color: #ffffff;
+}
+ul {
+	margin-top: 20px;
+	li {
+		font-size: 1em;
+		border: none;
+		span {
+			
+		}
+	}
+}
 </style>
