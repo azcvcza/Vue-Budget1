4f83b18d4749198a31866280fa59f0d11c20ca41
韦粤信 <283940802@qq.com>
2019-05-30 01:34:01 +0800

    feat:before irun,we need pass object to component

--- budget/src/app/budgets/components/CreateUpdateBudget.vue
+++ budget/src/app/budgets/components/CreateUpdateBudget.vue
@@ -10,105 +10,115 @@
 				</div>
 			</div>
 		</nav>
-		<div class="tile is-ancestor">
-			<div class="tile is-child notification is-primary">
-				<div v-if="editing">
-					<p class="title">now is editing budget</p>
-					<nav class="level">
-						<div class="level-item has-text-centered">
-							<div>
-								<p class="heading">Budgeted</p>
-								<p class="title">${{ selectedBudget.budgeted }}</p>
-							</div>
-						</div>
-						<div class="level-item has-text-centered">
-							<div>
-								<p class="heading">Spent</p>
-								<p class="title">${{ selectedBudget.spent }}</p>
-							</div>
-						</div>
-						<div class="level-item has-text-centered">
-							<div>
-								<p class="heading">Income</p>
-								<p class="title">${{ selectedBudget.income }}</p>
-							</div>
-						</div>
-					</nav>
 
-					<table class="table is-bordered">
-						<thead>
-							<tr>
-								<th>Category</th>
-								<th>Budgeted</th>
-								<th>Spent</th>
-								<th>Remaining</th>
-							</tr>
-						</thead>
-						<tbody>
-							<template>
-								<!--v-for="value, key in selectedBudget.budgetCategories" this doesn't work-->
-            <component
-              :is="budgetCategoryComponent(value)"
-              v-model="value"
-              v-on:update-budget-category="saveBudgetCategory"
-              v-on:edit-budget-category="activeBudgetCategory = value"
-            ></component>
-          </template>
-							<CreateUpdateBudgetCategory v-on:add-budget-category="addBudgetCategory"></CreateUpdateBudgetCategory>
-						</tbody>
-						<tfoot>
-							<tr>
-								<td></td>
-								<td>${{ selectedBudget.budgeted }}</td>
-								<td>${{ selectedBudget.spent }}</td>
-								<td>${{ selectedBudget.budgeted - selectedBudget.spent }}</td>
-							</tr>
-						</tfoot>
-					</table>
-				</div>
-				<div v-else>
-					<p class="title">now is creating budget</p>
-				</div>
-			</div>
-			<div class="tile is-child notification is-info">
+		<div class="columns">
+			<div class="column is-6">
 				<form class="form" @submit.prevent="processSave">
 					<label for="month" class="label">Month</label>
-					<p class="control">
+					<p class="control has-icon has-addons">
 						<datepicker
 							name="month"
 							input-class="input"
 							format="MMMM yyyy"
 							v-model="selectedBudget.month"
 						></datepicker>
-						<span class="icon">
-							<i class="fa fa-calendar" aria-hidden></i>
-						</span>
 					</p>
-					<p class="control">Budgeted: ${{ selectedBudget.budgeted }}</p>
-					<p class="control">Spent: ${{ selectedBudget.spent }}</p>
-					<p class="control">Income: ${{ selectedBudget.income }}</p>
-					<p class="help" v-if="!editing">To add budget items you must first save the budget.</p>
+					<span class="help" v-if="!editing">To add budget items you must first save the budget.</span>
+
 					<div class="field is-grouped">
 						<div class="control">
-							<button class="button is-link">Submit</button>
+							<span class="icon">
+								<i class="fa fa-calendar" aria-hidden="true"></i>
+							</span>
+
+							<button class="button is-primary" v-if="!this.editing">Submit</button>
+							<button class="button is-primary" v-else>Update</button>
 						</div>
 						<div class="control">
 							<router-link :to="{ name: 'budgetsList' }">
-								<button class="button is-warning">Cancel</button>
+								<button class="button is-link">Cancel</button>
 							</router-link>
 						</div>
 					</div>
 				</form>
 			</div>
 		</div>
+
+		<div v-if="editing">
+			<nav class="level">
+				<div class="level-item has-text-centered">
+					<div>
+						<p class="heading">Budgeted</p>
+						<p class="title">${{ selectedBudget.budgeted }}</p>
+					</div>
+				</div>
+				<div class="level-item has-text-centered">
+					<div>
+						<p class="heading">Spent</p>
+						<p class="title">${{ selectedBudget.spent }}</p>
+					</div>
+				</div>
+				<div class="level-item has-text-centered">
+					<div>
+						<p class="heading">Income</p>
+						<p class="title">${{ selectedBudget.income }}</p>
+					</div>
+				</div>
+			</nav>
+
+			<table class="table is-bordered">
+				<thead>
+					<tr>
+						<th>Category</th>
+						<th>Budgeted</th>
+						<th>Spent</th>
+						<th>Remaining</th>
+						<th></th>
+					</tr>
+				</thead>
+				<tbody>
+					 <div
+            v-for="(value, key) in selectedBudget.budgetCategories"
+          >
+					{{value}}+{{key}}
+            <!--<component
+              :is="budgetCategoryComponent(value)"
+              v-model="value"
+              v-on:update-budget-category="saveBudgetCategory"
+              v-on:edit-budget-category="activeBudgetCategory = value"
+            ></component>-->
+					</div>
+					<CreateUpdateBudgetCategory v-on:add-budget-category="addBudgetCategory"></CreateUpdateBudgetCategory>
+				</tbody>
+				<tfoot>
+					<tr>
+						<td>
+							Copy entire budget from:
+							<select
+								class="select"
+								@change="processDuplicateBudget($event.target.value)"
+							>
+								<option v-for="value, key in sortedBudgets" :value="value.id">{{ value.month | moment }}</option>
+							</select>
+						</td>
+						<td>${{ selectedBudget.budgeted }}</td>
+						<td>${{ selectedBudget.spent }}</td>
+						<td>${{ selectedBudget.budgeted + selectedBudget.spent }}</td>
+						<td></td>
+					</tr>
+				</tfoot>
+			</table>
+		</div>
 	</div>
 </template>
 
 <script>
-	import BudgetCategory from "./BudgetCategory";
-	import { mapActions, mapGetters } from "vuex";
+	import { mapActions, mapGetters, mapState } from "vuex";
 	import Datepicker from "vuejs-datepicker";
 	import CreateUpdateBudgetCategory from "./CreateUpdateBudgetCategory";
+	import BudgetCategory from "./BudgetCategory";
+	import { moment } from "../../../filter";
+	import { sortObjects } from "../../../utils";
 	export default {
 		name: "budget-create-edit-view",
 		components: {
@@ -120,8 +130,12 @@
 			return {
 				selectedBudget: {},
 				editing: false,
-				activeBudgetCategory: null
+				activeBudgetCategory: null,
+				lastBudget: null
 			};
+		},
+		filters: {
+			moment
 		},
 		mounted() {
 			if ("budgetId" in this.$route.params) {
@@ -130,6 +144,7 @@
 					if (selectedBudget) {
 						this.editing = true;
 						this.selectedBudget = Object.assign({}, selectedBudget);
+						console.log("while mounting,selectedBudget:", this.selectedBudget);
 					}
 				});
 			}
@@ -140,7 +155,8 @@
 				"updateBudget",
 				"loadBudgets",
 				"createBudgetCategory",
-				"updateBudgetCategory"
+				"updateBudgetCategory",
+				"duplicateBudget"
 			]),
 			resetAndGo() {
 				this.selectedBudget = {};
@@ -201,10 +217,30 @@
 					this.activeBudgetCategory === budgetCategory
 					? "create-update-budget-category"
 					: "budget-category";
+			},
+			processDuplicateBudget(budgetId) {
+				if (
+					confirm(
+						"Are you sure you want to duplicate that budget? Doing this will overwrite all existing data for this month (transaction data will NOT be erased)."
+					)
+				) {
+					this.duplicateBudget({
+						budget: this.selectedBudget,
+						baseBudget: this.getBudgetById(budgetId)
+					}).then(budget => {
+						this.selectedBudget = budget;
+					});
+				}
 			}
 		},
 		computed: {
-			...mapGetters(["getBudgetById", "getCategoryById"])
+			...mapGetters(["getBudgetById", "getCategoryById"]),
+			...mapState({
+				budgets: state => state.budgets.budgets
+			}),
+			sortedBudgets() {
+				return sortObjects(this.budgets, "month", true);
+			}
 		}
 	};
 </script>
--- budget/src/app/budgets/vuex/actions.js
+++ budget/src/app/budgets/vuex/actions.js
@@ -40,6 +40,44 @@ export const updateBudget = ({ commit, state }, data) => {
     saveBudget(data);
 };
 
+export const duplicateBudget = ({ commit, dispatch, getters }, data) => {
+    /*
+     * Expects an existing budget object, budget, and an budget to be copied, baseBudget
+     * Duplicates all budget categories and budgeted amounts to the new budget
+     */
+    if (!(data.budget && data.baseBudget)) return Promise.reject(new Error('Incorrect data sent to duplicateBudget'));
+
+    // clone our object in case we received something from the store
+    let budget = Object.assign({}, data.budget);
+
+    // let's reset some information first
+    budget.budgeted = 0;
+    budget.budgetCategories = null;
+    // note that we don't reset the spent or income because we aren't
+    // changing any transactions, which are what determine those values
+
+    commit('UPDATE_BUDGET', { budget: budget });
+
+    budget = getters.getBudgetById(budget.id);
+
+    if ('budgetCategories' in data.baseBudget) {
+        Object.keys(data.baseBudget.budgetCategories).forEach((key) => {
+            dispatch('createBudgetCategory', {
+                budget: budget,
+                budgetCategory: {
+                    category: data.baseBudget.budgetCategories[key].category,
+                    budgeted: data.baseBudget.budgetCategories[key].budgeted,
+                    spent: 0 // TODO: grab this value when we have transactions!
+                }
+            });
+        });
+    }
+
+    saveBudget(budget);
+
+    return budget;
+};
+
 export const loadBudgets = ({ state, commit }) => {
     if (!state.budgets || Object.keys(state.budgets).length === 0) {
         return fetchBudgets().then((res) => {
@@ -96,6 +134,7 @@ export const createBudgetCategory = ({ commit, dispatch, getters }, data) => {
         value: budgetCategory.budgeted
     });
 };
+
 export const updateBudgetCategory = ({ commit, dispatch, getters }, data) => {
     let newBudget = data.budgetCategory.budgeted;
     let oldBudget = getters.getBudgetCategoryById(data.budget.id, data.budgetCategory.id).budgeted;
@@ -109,6 +148,21 @@ export const updateBudgetCategory = ({ commit, dispatch, getters }, data) => {
     }
 
     commit('UPDATE_BUDGET_CATEGORY', data);
+
+    // save using the budget in our store
+    saveBudget(getters.getBudgetById(data.budget.id));
+};
+
+export const updateBudgetCategorySpent = ({ commit, dispatch, getters }, data) => {
+    // expects data.budget, data.budgetCategory, and data.spent
+    // spent should always be the amount spent on a transaction, not a total amount
+    commit('UPDATE_BUDGET_CATEGORY_BALANCE', { budget: data.budget, value: data.amount, budgetCategory: data.budgetCategory, param: 'spent' });
+
+    dispatch('updateBudgetBalance', {
+        budget: data.budget,
+        param: 'spent',
+        value: data.budget.spent + data.amount
+    });
 
     // save using the budget in our store
     saveBudget(getters.getBudgetById(data.budget.id));
